00eabab24759033210d79894a4b477fb583ab100
PhantomOz <faniogor@gmail.com>
2020-10-16 18:53:07 +0100

    first-major-task

---
+++ MajorTask.php
@@ -0,0 +1,66 @@
+<?php
+
+    //**************** Declare your variables below this comment *********************
+
+
+
+
+
+    /* 
+        ==================
+
+        ++++++++++++ Major Task For this Week Friday, 16th - Oct - 2020 +++++++++++++
+
+        Perform arithemetic operation on each operators in the below code.
+
+        Make sure to upload this file to your github repository.
+
+        Deadline line for submission is 9pm on the same day.
+
+        **********************************************
+        !!!!!!!!!! Have a lovely Weekend !!!!!!!!!!!!!
+
+        ==================
+    */
+
+    $num1 = 23;
+    $num2 = 10;
+
+    //Addition
+    echo "<h1>Addition</h1>";
+    echo "<p>This program adds two variables together</p>";
+    $sum = $num1 + $num2;
+    echo "$num1 + $num2 = ". $sum; 
+
+
+    //subtraction
+    echo "<h1>Subtraction</h1>";
+    echo "<p>This program subtract two variables from each other</p>";
+    $sum = $num1 - $num2;
+    echo "$num1 - $num2 = ". $sum; 
+
+
+    //multiplication
+    echo "<h1>Multiplication</h1>";
+    echo "<p>This program multiply two variables together</p>";
+    $sum = $num1 * $num2;
+    echo "$num1 * $num2 = ". $sum; 
+
+
+    //Division
+    echo "<h1>Division</h1>";
+    echo "<p>This program divides two variables</p>";
+    $sum = $num1 / $num2;
+    echo "$num1 / $num2 = ". $sum; 
+
+
+    //Modulus
+    echo "<h1>Modulus</h1>";
+    echo "<p>This program divides two variables and return their remainder if there is any.</p>";
+    $sum = $num1 % $num2;
+    echo "$num1 % $num2 = ". $sum; 
+
+
+
+
+?>
